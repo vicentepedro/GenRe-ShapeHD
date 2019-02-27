5b5e993e3124e82e978611fbb2df9c4098449bc2
Doroana <joaopnpdoroana@gmail.com>
2019-02-27 12:39:39 +0000

    [full_genre] change batch size and dataset

--- scripts/train_full_genre.sh
+++ scripts/train_full_genre.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-outdir=./output/genre
-inpaint_path=/path/to/trained/inpaint.pt
-
+#outdir=./output/genre
+#inpaint_path=/path/to/trained/inpaint.pt
+outdir=/media/Data/dsl-course/GenRe_Training/output/genre
+inpaint_path=/media/Data/dsl-course/GenRe_Training/output/inpaint/depth_pred_with_sph_inpaint_ycb_genre_0.0001_all/0/best.pt
 if [ $# -lt 2 ]; then
     echo "Usage: $0 gpu class[ ...]"
     exit 1
@@ -19,11 +20,11 @@ source activate shaperecon
 python train.py \
     --net genre_full_model \
     --pred_depth_minmax \
-    --dataset shapenet \
+    --dataset ycb_genre \
     --classes "$class" \
-    --batch_size 4 \
-    --epoch_batches 1000 \
-    --eval_batches 30 \
+    --batch_size 16 \
+    --epoch_batches 350 \
+    --eval_batches 10 \
     --log_time \
     --optim adam \
     --lr 1e-4 \
